659c21741fa787e4b2fd49d1aabe535f81923467
togi06 <t_bayter_06@hotmail.com>
2023-03-15 20:01:57 +0100

    mülakat sorusu 1

--- Queries/mülakat1.sql
+++ Queries/mülakat1.sql
@@ -7,13 +7,14 @@ order by SALARY desc )
 where ROWNUM <15;
 
 -- distinct kullanmalıyız cunku bazı rakamları birkaç kişi kazanıyor
+-- we use "distinct" because of removing the duplicate values
 select *
 from(select distinct  SALARY from EMPLOYEES
      order by SALARY desc )
 where ROWNUM <15;
 
 -- 14. en yüksek kazanılan miktar --
-select min(SALARY)
+select min(SALARY) -- min yapmamızın sebebi ilk 14'teki en az kazananı göstermek için
 from(select distinct SALARY from EMPLOYEES
      order by SALARY desc )
 where ROWNUM <15;
