73986c38dd584644079986911531347d706247f5
togi06 <t_bayter_06@hotmail.com>
2023-04-02 15:14:26 +0200

    add Creat Table

--- Queries/joinsExample2.sql
+++ Queries/joinsExample2.sql
@@ -15,3 +15,11 @@ select  FIRST_NAME, LAST_NAME, DEPARTMENT_NAME,CITY
 from EMPLOYEES e join  DEPARTMENTS d
 on e.DEPARTMENT_ID = d.DEPARTMENT_ID
  join LOCATIONS l on d.LOCATION_ID =  l.LOCATION_ID;
+
+-- get me first_name, last_name and department_name ,city, country_name for all employees
+
+select FIRST_NAME,LAST_NAME,DEPARTMENT_NAME,CITY,COUNTRY_NAME
+from EMPLOYEES e join DEPARTMENTS d on e.DEPARTMENT_ID = d.DEPARTMENT_ID
+join LOCATIONS l on  d.LOCATION_ID =l.LOCATION_ID
+join COUNTRIES c on c.COUNTRY_ID = l.COUNTRY_ID;
+
