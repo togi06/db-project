c5e51ab1ad043a9fe4deb4c3e958261baa41bd6d
togi06 <t_bayter_06@hotmail.com>
2023-04-23 12:17:26 +0200

    add Creat Table

---
+++ Queries.short/SelectStatement_Distinct.sql
@@ -0,0 +1,47 @@
+select * from  DEPARTMENTS;
+
+select DEPARTMENT_NAME from DEPARTMENTS;
+
+select DEPARTMENT_NAME, DEPARTMENT_ID from DEPARTMENTS;
+
+/*
+ select ten sonra "ctrl + space" yaparsan o database deki kullanabileceğin
+ tüm sıraları görebilirsin.
+Ama bunu yapmadan önce "from "table" yapmak zorundasın!!
+ */
+select DEPARTMENT_NAME,DEPARTMENT_ID from DEPARTMENTS;
+
+
+/*
+ birden fazla tablodan da bilgi çağırabiliyoruz
+
+
+ */
+select * from EMPLOYEES;
+select * from  DEPARTMENTS;
+
+select  * from EMPLOYEES, DEPARTMENTS;
+
+/*
+ birden fazla tablodan birden fazla bilgi çağırabiliyoruz
+ */
+select EMPLOYEES.EMPLOYEE_ID, DEPARTMENTS.DEPARTMENT_ID from  EMPLOYEES,DEPARTMENTS;
+
+
+/*                 
+ distinct keyword: displays the result without the duplicate
+ distinct keyword: makes duplicates information  to single
+ */
+
+ select distinct DEPARTMENT_ID from DEPARTMENTS;
+
+select  * from  EMPLOYEES;
+
+select  SALARY from  EMPLOYEES;
+select distinct  SALARY from  EMPLOYEES;
+
+select  DEPARTMENT_ID from EMPLOYEES;
+select  distinct DEPARTMENT_ID from EMPLOYEES;
+
+select *from jobs;
+select distinct MAX_SALARY from JOBS;
