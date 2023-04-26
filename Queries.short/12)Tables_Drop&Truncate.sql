0300d916638d2af243abd87fa17baf3185e519d3
togi06 <t_bayter_06@hotmail.com>
2023-04-26 11:47:19 +0200

    add new statement

---
+++ Queries.short/12)Tables_Drop&Truncate.sql
@@ -0,0 +1,14 @@
+/*
+ Droping table: tabloyu sıler
+    drop table TableName
+
+ Truncate table ; tablodaki bilgileri siler
+ truncate table ADDRESS1;
+ */
+
+select  * from  CUSTOMER2;
+drop  table  CUSTOMER2;
+
+
+select *  from ADDRESS1;
+
